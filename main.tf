9ca961b5f8c1b9b53f61c95dd5bad32ab6213d9a
luismayta <luis@hadenlabs.com>
2021-10-10 08:55:16 -0500

    chore: implement value default (#5)

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
   input = {
     username                = var.username
     first_name              = var.first_name
-    path                    = var.path == null ? local.defaults.path : var.path
+    path                    = try(var.path, local.defaults.path)
     public_key              = var.public_key
     password_reset_required = try(var.password_reset_required, local.defaults.password_reset_required)
   }
